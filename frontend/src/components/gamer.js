c604d68c24097d73ca10cf0d3172f071d7d0766e
Andrew KIl <kil.andrew@gmail.com>
2019-03-27 18:51:54 -0400

    scoring system logic

--- frontend/src/components/gamer.js
+++ frontend/src/components/gamer.js
@@ -11,8 +11,10 @@ class Gamer extends Component {
     userInput: "",
     go: false,
     playerstate: 2,
-    error: "go on.."
+    error: "go on..",
+    points: 0
   };
+
   componentDidMount() {
     this.setState({
       //ace
@@ -59,28 +61,13 @@ class Gamer extends Component {
     //     })
   }
   handleChange = e => {
+    console.log(e);
     this.setState({
       [e.target.id]: e.target.value
     });
     console.log("length", this.state.userInput.length);
     console.log(this.state.userInput);
     console.log(this.state.definition[this.state.userInput.length]);
-    if (
-      this.state.userInput[this.state.userInput.length - 1] ===
-      this.state.definition[this.state.userInput.length - 1]
-    ) {
-      this.setState({
-        error: "go on.."
-      });
-    } else {
-      this.setState({
-        error: "u typed something wrong"
-      });
-    }
-
-    if (this.state.userInput.length === this.state.definition.length) {
-      this.checkEqual();
-    }
   };
 
   startButton = e => {
@@ -88,6 +75,7 @@ class Gamer extends Component {
       go: true
     });
   };
+
   checkEqual = () => {
     if (this.state.userInput === this.state.definition) {
       this.setState({
@@ -104,24 +92,52 @@ class Gamer extends Component {
     }
   };
 
+  handleKey = e => {
+    console.log(e.key);
+
+    if (e.key === "Backspace") {
+      this.setState({ points: this.state.points - 2 });
+    } else if (
+      this.state.userInput[this.state.userInput.length - 1] ===
+      this.state.definition[this.state.userInput.length - 1]
+    ) {
+      this.setState({
+        error: "go on..",
+        points: this.state.points + 2
+      });
+    } else {
+      this.setState({
+        error: "u typed something wrong",
+        points: this.state.points - 1
+      });
+    }
+
+    if (this.state.userInput.length === this.state.definition.length) {
+      this.checkEqual();
+    }
+  };
+
   render() {
     return (
       <div className="App">
         <Countdown
           startbutton={this.startButton}
           handleChange={this.handleChange}
+          textLength={this.state.definition.length}
         />
-        <h1 id="word">{this.state.word}</h1>
-        <p id="type">{this.state.type}</p>
+        <h3>{this.state.points} point(s)</h3>
+
         <button onClick={this.startButton} id="start-button">
-          Start
+          Start Typing
         </button>
         <br />
+        <h1 id="word">{this.state.word}</h1>
+        <p id="type">{this.state.type}</p>
         {this.state.go ? (
           <input
             id="userInput"
             autoComplete="off"
-            onKeyDown={this.spaceCheck}
+            onKeyDown={this.handleKey}
             onChange={this.handleChange}
             placeholder="Type the word definition here"
             value={this.state.userInput}
