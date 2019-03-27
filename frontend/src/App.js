e6f14c7045ea64c6c435cc3bbe15dc1d3e17d411
your name here <your_email_here>
2019-03-27 15:48:56 -0400

    cleared out console logs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,44 +1,39 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { Component } from 'react';
+import './App.css';
 // import axios from 'axios'
-import WordDisplay from "./WordDisplay";
-
-// let headers = {
-//   "Accept": "application/json",
-//   "app_id": "0296bd0a",
-//   "app_key": "5a0766a941eb5287bafeab007dc348c8"
-// }
+import WordDisplay from './WordDisplay'
 
 // type word then when space is pushed is added to array
 // then arrayInput is compared with definitionArray
 class App extends Component {
   state = {
-    word: "",
-    type: "",
-    example: "",
-    definition: "",
-    userInput: "",
-    go: false,
-    playerstate: 2,
-    error: "go on.."
-  };
-  componentDidMount() {
+    word:'',
+    type:'',
+    example:'',
+    definition:'',
+    userInput:'',
+    go:false,
+    playerstate:2,
+    error:'go on..'
+
+  }
+  componentDidMount(){
     this.setState({
       //ace
-      word: "ace",
+      word:'ace',
       // word:res.data.results[0].lexicalEntries[0].text,
 
       //NOUN
-      type: "noun",
+      type:'noun',
       // type:res.data.results[0].lexicalEntries[0].lexicalCategory,
+
 
       example: "life had started dealing him aces again",
       // example:res.data.results[0].lexicalEntries[0].entries[0].senses[0].examples[0].text,
 
-      definition:
-        "a playing card with a single spot on it, ranked as the highest card in its suit in most card games example"
+      definition:"a playing card with a single spot on it, ranked as the highest card in its suit in most card games example"
       // definition:res.data.results[0].lexicalEntries[0].entries[0].senses[0].definitions[0],
-    });
+    })
     // axios
     //   .get('https://cors-anywhere.herokuapp.com/https://od-api.oxforddictionaries.com:443/api/v1/entries/en/ace',{ headers: {
     //     "Accept": "application/json",
@@ -66,16 +61,13 @@ class App extends Component {
     //       })
     //
     //     })
-<<<<<<< HEAD
 
   }
   handleChange = e => {
     this.setState({
       [e.target.id]:e.target.value
     })
-    console.log('length',this.state.userInput.length);
-    console.log(this.state.userInput)
-    console.log(this.state.definition[this.state.userInput.length]);;
+
     if((this.state.userInput[this.state.userInput.length-1])===(this.state.definition[this.state.userInput.length-1])){
 
 
@@ -84,122 +76,55 @@ class App extends Component {
       })
 
     }else{
-=======
-    console.log("THIS IS DEF", this.state.definition);
-  }
-  handleChange = e => {
-    this.setState({
-      [e.target.id]: e.target.value
-    });
-    if (
-      this.state.userInput[this.state.userInput.length - 1] ===
-      this.state.definition[this.state.userInput.length - 1]
-    ) {
-      console.log("wooaahh");
       this.setState({
-        error: "go on.."
-      });
-    } else {
-      console.log("try again", this.state.definition.length);
->>>>>>> 8071be4e863a5403fa298ac23bcae29890e87c71
-      this.setState({
-        error: "u typed something wrong"
-      });
+        error:"u typed something wrong"
+      })
     }
-<<<<<<< HEAD
     if(this.state.userInput.length===this.state.definition.length){
       this.checkEqual()
     }
 
-
   }
-=======
-    if (this.state.userInput.length - 1 === this.state.definition.length - 1) {
-      this.checkEqual();
-    }
-
-    // (this.state.definition[this.state.userInput.length-1])
-  };
->>>>>>> 8071be4e863a5403fa298ac23bcae29890e87c71
   startButton = e => {
     this.setState({
-      go: true
-    });
-  };
+      go:true
+    })
+
+  }
   checkEqual = () => {
-    if (this.state.userInput === this.state.definition) {
+
+    if(this.state.userInput===this.state.definition){
       this.setState({
-        playerState: 1
-      });
-    } else if (this.state.userInput !== this.state.definition) {
+        playerState:1,
+
+      })
+    }else if(this.state.userInput!==this.state.definition){
       this.setState({
-        playerState: 0
-      });
-    } else {
+        playerState:0,
+
+      })
+    }else{
       this.setState({
-        playerState: 2
-      });
+        playerState:2,
+
+      })
     }
-<<<<<<< HEAD
   }
 
-=======
-  };
-  arrayify = def => {
-    let out = def.split(" ");
-    console.log(out);
-  };
-  check = e => {
-    // if(e.keyCode === 32){
-    //     console.log("space has been clicked");
-    // }
-  };
->>>>>>> 8071be4e863a5403fa298ac23bcae29890e87c71
 
   render() {
-
 
     return (
       <div className="App">
         <h1>{this.state.word}</h1>
         <p>{this.state.type}</p>
-<<<<<<< HEAD
         <button onClick={this.startButton}>Start</button><br/>
         {this.state.go?
           <input  id='userInput'  autoComplete="off" onKeyDown={this.spaceCheck} onChange={this.handleChange} placeholder='Type the word definition here' value={this.state.userInput} type='text'/>:
           <h1>Press Start To begin</h1>}
-=======
-        <button onClick={this.startButton}>Start</button>
-        <br />
-        {this.state.go ? (
-          <input
-            id="userInput"
-            onKeyDown={this.spaceCheck}
-            onChange={this.handleChange}
-            placeholder="Type the word definition here"
-            value={this.state.userInput}
-            type="text"
-          />
-        ) : (
-          <h1>Press Start To begin</h1>
-        )}
->>>>>>> 8071be4e863a5403fa298ac23bcae29890e87c71
         <h3>{this.state.error}</h3>
-        <WordDisplay
-          go={this.state.go}
-          word={this.state.word}
-          userInput={this.state.userInput}
-          type={this.state.type}
-          definition={this.state.definition}
-          example={this.state.example}
-        />
-        {this.state.playerState === 1 ? (
-          <h1>You have won</h1>
-        ) : this.state.playerState === 0 ? (
-          <h1>You have lost</h1>
-        ) : (
-          ""
-        )}
+        <WordDisplay go={this.state.go} word={this.state.word} userInput={this.state.userInput} type={this.state.type} definition={this.state.definition} example={this.state.example}/>
+        {this.state.playerState===1?<h1>You have won</h1>:this.state.playerState===0?<h1>You have lost</h1>:""}
       </div>
     );
   }
