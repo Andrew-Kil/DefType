defc1de1babdbccb66e25559bdec1311dea6ef46
your name here <your_email_here>
2019-03-27 14:46:02 -0400

    Added Logic and basic functionality for game

---
+++ frontend/src/WordDisplay.js
@@ -0,0 +1,22 @@
+import React, { Component} from 'react'
+import './App.css'
+
+export default class WordDisplay extends Component {
+  state = {
+
+  }
+  render(){
+    // console.log(this.props);
+    // <h1>Word: </h1><p>{this.props.word}</p>
+    return(
+    <div>
+
+
+
+      <h1 >{this.props.userInput}</h1>
+      <h3>definition</h3><p>{this.props.definition}</p>
+      <h3>example</h3><p>{this.props.example}</p>
+      </div>
+    )
+  }
+}
