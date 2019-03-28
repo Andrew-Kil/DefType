22a14bd3ee5cf17c46600b344975f76f82c2586f
Jean Max Mezalon <jeanmaxmezalon@pursuit.org>
2019-03-28 15:08:33 -0400

    added color to timer

--- frontend/src/components/WordDisplay.js
+++ frontend/src/components/WordDisplay.js
@@ -1,20 +1,21 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
 export default class WordDisplay extends Component {
-  state = {};
-  render() {
+  state = {
+
+  }
+  render(){
     // console.log(this.props);
     // <h1>Word: </h1><p>{this.props.word}</p>
-    return (
-      <div className="gamer-container">
-        <div id="input-container">
-          <h1>{this.props.userInput}</h1>
-        </div>
-        <h3 id="definition">definition</h3>
-        <p>{this.props.definition}</p>
-        <h3 id="example">example</h3>
-        <p>{this.props.example}</p>
+    return(
+    <div>
+
+
+
+      <h1>{this.props.userInput}</h1>
+      <h3 id="definition">definition</h3><p>{this.props.definition}</p>
+      <h3 id="example">example</h3><p>{this.props.example}</p>
       </div>
-    );
+    )
   }
 }
