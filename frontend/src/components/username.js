c81d38c57818c25230f66e68610144efcb10d9a2
Andrew KIl <kil.andrew@gmail.com>
2019-03-28 11:30:55 -0400

    stuff

--- frontend/src/components/username.js
+++ frontend/src/components/username.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-class Home extends Component{
-    render(){
-        return(
-            <div>
-                <div>
-                    <Link to='/'>Home </Link>
-                    <Link to='/gamer'> Game </Link>
-                </div>
-                <div>
-                    Please enter your username:
-                </div>
-                <br></br>
-                <br></br>
-                <form>
-                    <input type='text' placeholder='username'/>
-                    <button type='submit'>Submit</button>
-                </form>
-                
-                <Link to='/gamer'>Start Game</Link>
-            </div>
-        )
-    }
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <Link to="/">Home </Link>
+          <Link to="/gamer"> Game </Link>
+        </div>
+        <div>Please enter your username:</div>
+        <br />
+        <br />
+        <form>
+          <input type="text" placeholder="username" />
+          <button type="submit">Submit</button>
+        </form>
+
+        <Link to="/gamer">Start Game</Link>
+      </div>
+    );
+  }
 }
 
 export default Home;
