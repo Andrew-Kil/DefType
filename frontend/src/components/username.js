c80224d31100f93ab3dbc8991ded2cf44d3deda0
Andrew KIl <kil.andrew@gmail.com>
2019-03-28 11:25:57 -0400

    changes to username branch

--- frontend/src/components/username.js
+++ frontend/src/components/username.js
@@ -1,28 +1,56 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-class Home extends Component{
-    render(){
-        return(
-            <div>
-                <div id='n'>
-                    <Link to='/'>Home </Link>
-                    <Link to='/gamer'> Game </Link>
-                </div>
-                <div>
-                    Please enter your username:
-                </div>
-                <br></br>
-                <br></br>
-                <form id='user-form'>
-                    <input id='user-input'type='text' placeholder='username'/>
-                    <button id='user-input' type='submit'>Submit</button>
-                </form>
-                
-                <Link to='/gamer'>Start Game</Link>
-            </div>
-        )
-    }
+class Home extends Component {
+  state = {
+    name: "",
+    message: "Welcome, ",
+    submitted: false
+  };
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({
+      name: e.target.value,
+      submitted: true,
+      message: this.state.message + this.state.name
+    });
+  };
+
+  render() {
+    console.log(this.state);
+    return (
+      <div>
+        <div id="n">
+          <Link to="/">Home </Link>
+          <Link to="/gamer"> Game </Link>
+        </div>
+        <div>Please enter your username:</div>
+        <br />
+        <br />
+        <form id="user-form">
+          <input
+            id="user-input"
+            type="text"
+            placeholder="username"
+            name="name"
+            onChange={this.handleChange}
+          />
+          <button id="user-input" type="submit" onSubmit={this.handleSubmit}>
+            Submit
+          </button>
+        </form>
+
+        <p>{this.state.submitted ? this.state.message : null}</p>
+
+        <Link to="/gamer">Start Game</Link>
+      </div>
+    );
+  }
 }
 
 export default Home;
